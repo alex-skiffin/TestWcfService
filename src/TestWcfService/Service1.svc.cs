813982d7b38d1e08c328a65d328a7316322dd01f
Mekhonoshin <mekhonoshin@macroscop.com>
2017-08-24 08:01:29 +0500

    Init commit

---
+++ src/TestWcfService/Service1.svc.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace TestWcfService
+{
+	// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+	// NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+	public class Service1 : IService1
+	{
+		public List<UserInfo> GetUsers(string format)
+		{
+			List<UserInfo> users = new List<UserInfo>();
+			users.Add(new UserInfo {Login = "123"});
+			return users;
+		}
+
+		public string GetData(int value)
+		{
+			return string.Format("You entered: {0}", value);
+		}
+
+		public CompositeType GetDataUsingDataContract(CompositeType composite)
+		{
+			if (composite == null)
+			{
+				throw new ArgumentNullException("composite");
+			}
+			if (composite.BoolValue)
+			{
+				composite.StringValue += "Suffix";
+			}
+			return composite;
+		}
+	}
+}
